a0415cafdf6dcb3177565d4a7ec1679cc8d5fb70
Andrew McCann <andrewm@getjobber.com>
2016-06-23 21:27:10 -0600

    Initialized Package

---
+++ spec/gh-pull-request-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GhPullRequestView from '../lib/gh-pull-request-view';
+
+describe('GhPullRequestView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
